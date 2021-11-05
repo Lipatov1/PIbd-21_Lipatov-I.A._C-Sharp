754c53bd1b097ab1df0563a7a9318ff7cc6464ee
Lipatov1 <Ilya773@vk.com>
2021-11-05 17:54:09 +0400

    Лабораторная работа №6

--- WindowsFormsTechnic/WindowsFormsTechnic/BaseCollection.cs
+++ WindowsFormsTechnic/WindowsFormsTechnic/BaseCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace WindowsFormsTechnic {
     // Класс-коллекция баз
@@ -13,6 +14,9 @@ namespace WindowsFormsTechnic {
         // Ширина и высота окна отрисовки
         private readonly int pictureWidth;
         private readonly int pictureHeight;
+
+        // Разделитель для записи информации в файл
+        private readonly char separator = ':';
 
         // Конструктор
         public BaseCollection(int pictureWidth, int pictureHeight) {
@@ -43,6 +47,67 @@ namespace WindowsFormsTechnic {
                     return baseStages[ind];
                 }
                 return null;
+            }
+        }
+
+        // Сохранение информации по автомобилям на парковках в файл
+        public bool SaveData(string filename) {
+            using (StreamWriter sw = new StreamWriter(filename)) {
+                sw.WriteLine("BaseCollection");
+                foreach (var level in baseStages) {
+                    sw.WriteLine("Base" + separator + level.Key);
+
+                    ITransport militaryEquipment = null;
+                    for (int i = 0; (militaryEquipment = level.Value.GetNext(i)) != null; i++) {
+                        if (militaryEquipment != null)  {
+                            if (militaryEquipment.GetType().Name == "ArmoredCar") {
+                                sw.Write("ArmoredCar" + separator);
+                            } else if (militaryEquipment.GetType().Name == "SelfPropArtilleryInstal") {
+                                sw.Write("SelfPropArtilleryInstal" + separator);
+                            }
+
+                            sw.WriteLine(militaryEquipment);
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+        // Загрузка информации по автомобилям на парковках из файла
+        public bool LoadData(string filename) {
+            if (!File.Exists(filename)) {
+                return false;
+            }
+
+            using (StreamReader sr = new StreamReader(filename)) {
+                if (sr.ReadLine().Contains("BaseCollection")) {
+                    baseStages.Clear();
+                } else {
+                    return false;
+                }
+
+                Vehicle militaryEquipment = null;
+                string key = string.Empty;
+                string line;
+
+                for (int i = 0; (line = sr.ReadLine()) != null; i++) {
+                    if (line.Contains("Base")) {
+                        key = line.Split(separator)[1];
+                        baseStages.Add(key, new Base<Vehicle>(pictureWidth, pictureHeight));
+                    } else if (line.Contains(separator)) {
+                        if (line.Contains("ArmoredCar")) {
+                            militaryEquipment = new ArmoredCar(line.Split(separator)[1]);
+                        } else if (line.Contains("SelfPropArtilleryInstal")) {
+                            militaryEquipment = new SelfPropArtilleryInstal(line.Split(separator)[1]);
+                        }
+
+                        if (baseStages[key] + militaryEquipment == -1) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
             }
         }
     }
